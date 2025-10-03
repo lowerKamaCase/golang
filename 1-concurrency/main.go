74b892a0d5e67b1b130b3a2bafb49cb68767243f
lowerKamaCase <lowerkamacase@gmail.com>
2025-10-03 13:06:03 +0300

    Check

--- 1-concurrency/main.go
+++ 1-concurrency/main.go
@@ -26,6 +26,8 @@ func square(count int, numChannel chan float64, channel chan float64) {
 	}
 }
 
+// just to be sure
+
 func createSlice(count int, ch chan float64) {
 	for i := 0; i < count; i++ {
 		ch <- math.Ceil(rand.Float64() * 100)
