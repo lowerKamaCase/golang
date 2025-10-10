33a0d7118d195f57e890a8185ee88714c2bb1414
lowerKamaCase <lowerkamacase@gmail.com>
2025-10-10 07:30:34 +0300

    Link added

---
+++ migrations/auto.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"lowerkamacase/golang/pkg/link"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.AutoMigrate(&link.Link{})
+}
