4af0a6037d94b2cbcd2b8cdfd1a2839bd2ca75aa
lowerKamaCase <lowerkamacase@gmail.com>
2025-10-28 22:50:31 +0300

    JWT added

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,8 +1,8 @@
 package auth
 
 import (
-	"fmt"
 	"lowerkamacase/golang/configs"
+	"lowerkamacase/golang/pkg/jwtInstance"
 	"lowerkamacase/golang/pkg/req"
 	"lowerkamacase/golang/pkg/res"
 	"net/http"
@@ -20,7 +20,7 @@ type AuthHandler struct {
 
 func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 	handler := &AuthHandler{
-		Config: deps.Config,
+		Config:      deps.Config,
 		AuthService: deps.AuthService,
 	}
 	router.HandleFunc("POST /auth/login", handler.Login())
@@ -34,25 +34,51 @@ func (handler *AuthHandler) Login() http.HandlerFunc {
 		if err != nil {
 			return
 		}
-		fmt.Println(body)
 
 		email, err := handler.AuthService.Login(body.Email, body.Password)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
 		}
-		
-		res.Json(w, email, 200)
+
+		token, err := jwtInstance.NewJWT(handler.Config.Auth.Secret).Create(email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		response := LoginResponse{
+			Token: token,
+		}
+
+		res.Json(w, response, http.StatusOK)
 	}
 
 }
 
 func (handler *AuthHandler) Register() http.HandlerFunc {
-	return func(rw http.ResponseWriter, request *http.Request) {
-		body, err := req.HandleBody[RegisterRequest](&rw, request)
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := req.HandleBody[RegisterRequest](&w, r)
 		if err != nil {
 			return
 		}
-		handler.AuthService.Register(body.Email, body.Password, body.Name)
 
+		email, err := handler.AuthService.Register(body.Email, body.Password, body.Name)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwtInstance.NewJWT(handler.Config.Auth.Secret).Create(email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		response := RegisterResponse{
+			Token: token,
+		}
+
+		res.Json(w, response, http.StatusOK)
 	}
 }
